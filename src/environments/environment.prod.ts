a63c8e5f3aa96d292262c842c7c6ce5a2c6dd57b
masterDev95 <masdevelopper@gmail.com>
2023-04-21 11:39:07 +0200

    :heavy_plus_sign: axonaut clé API

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  axonautApiKey: '8f54de4083bb3dc3f7294278d680c251',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  axonautApiKey: '8f54de4083bb3dc3f7294278d680c251',
 };
 
 /*
