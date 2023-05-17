844a543e8289a2918a24625babc46dfc217f88b3
masterDev95 <masdevelopper@gmail.com>
2023-05-17 11:05:03 +0100

    :tada: Prep V1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
   production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyD9eI7uXWdX9TKzf-cFAGcqkqXbEv85Qvk",
+    authDomain: "stock-pix-a0923.firebaseapp.com",
+    projectId: "stock-pix-a0923",
+    storageBucket: "stock-pix-a0923.appspot.com",
+    messagingSenderId: "234415904204",
+    appId: "1:234415904204:web:5c0d8ea7e930831ee3f969",
+    measurementId: "G-LQ0J0HFKT6"
+  },
+  functionsID: 'yx4e5a7n2q',
 };
