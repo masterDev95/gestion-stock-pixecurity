7f8f54170dfe97ee407932b1921400d94ac6514c
masterDev95 <masdevelopper@gmail.com>
2023-08-21 12:12:35 +0200

    1.1.0 - Click droit et custom contextMenu

--- src/app/categories/liste/liste.component.ts
+++ src/app/categories/liste/liste.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChildren, QueryList, ElementRef, HostListener } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { IonicModule, NavController } from '@ionic/angular';
 import { Unsubscribe } from 'firebase/firestore';
@@ -11,77 +11,77 @@ import { CategorieService } from 'src/app/services/categorie.service';
   styleUrls: ['./liste.component.scss'],
   standalone: true,
   imports: [IonicModule, CommonModule, RouterModule],
-  template: `
-    <ion-row>
-      <ion-col
-        size="6"
-        sizeMd="4"
-        sizeLg="3"
-        *ngFor="let c of collectionCategories | keyvalue"
-      >
-        <ion-card
-          class="ion-text-center ion-text-uppercase"
-          button
-          (click)="openCategorie(c.key, c.value)"
-        >
-          <ion-card-header>
-            <ion-card-title>
-              <p>
-                <ion-icon
-                  slot="start"
-                  [name]="c.value.ionIcon"
-                  size="large"
-                ></ion-icon>
-              </p>
-            </ion-card-title>
-          </ion-card-header>
-          <ion-card-content>
-            <p>
-              {{ c.value.name }}
-            </p>
-            <p>
-              {{ c.value.listeIdProduits.length }}
-              {{ c.value.listeIdProduits.length > 1 ? 'articles' : 'article' }}
-              dispos
-            </p>
-          </ion-card-content>
-        </ion-card>
-      </ion-col>
-
-      <ion-col
-        *ngIf="Object.keys(collectionCategories).length < 1"
-        class="ion-padding ion-text-justify"
-      >
-        <p>
-          Il n'y a pour le moment aucune catégorie disponible, mais vous pouvez
-          en créer une si vous le souhaitez.
-        </p>
-        <div class="ion-text-center">
-          <ion-button size="large" [routerLink]="['ajout-categorie']">
-            <ion-icon slot="start" name="add"></ion-icon>
-            Nouveau
-          </ion-button>
-        </div>
-      </ion-col>
-    </ion-row>
-  `,
+  templateUrl: 'liste.component.html',
 })
 export class ListeComponent implements OnInit, OnDestroy {
+  @ViewChildren('targetElement') cardElements!: QueryList<
+    ElementRef<HTMLElement>
+  >;
+
   /** Collection des catégories. */
   collectionCategories: { [key: string]: Categorie } = {};
   /** Fonction de désabonnement de l'écoute des catégories. */
   unsubCategoriesListen!: Unsubscribe;
+  listeFonctionsContextMenu: {
+    name: string;
+    action: () => void;
+    icon?: string;
+    bold?: boolean;
+    iconColor?: string;
+  }[] = [];
+  menu!: HTMLElement;
 
   /** Objet pour utiliser la fonction Object dans le template. */
   Object = Object;
 
   constructor(
     private categorieService: CategorieService,
-    private navController: NavController
+    private navController: NavController,
   ) {}
 
   ngOnInit() {
+    this.menu = document.getElementById('custom-menu')!;
+
     this.initCategoriesListening();
+  }
+
+  @HostListener('window:click')
+  @HostListener('window:auxclick')
+  @HostListener('window:blur')
+  closeMenu() {
+    if (this.menu.classList.contains('show')) {
+      this.menu.classList.remove('show');
+    }
+  }
+
+  openCategorieContextMenu(
+    event: MouseEvent,
+    categorieId: string,
+    categorie: Categorie
+  ) {
+    event.preventDefault();
+
+    this.refreshContextMenu(categorieId, categorie);
+
+    this.menu.style.left = event.pageX - 12 + 'px';
+    this.menu.style.top = event.pageY - 56 + 'px';
+    this.menu.classList.add('show');
+  }
+
+  refreshContextMenu(categorieId: string, categorie: Categorie) {
+    this.listeFonctionsContextMenu = [
+      {
+        name: `Voir les produits`,
+        action: () => this.openCategorie(categorie),
+        bold: true,
+      },
+      {
+        name: `Modifier ${categorie.name}`,
+        action: () => this.editCategorie(categorieId, categorie),
+        icon: 'create-outline',
+        iconColor: 'warning',
+      },
+    ];
   }
 
   /**
@@ -103,13 +103,24 @@ export class ListeComponent implements OnInit, OnDestroy {
 
   /**
    * Ouvre la catégorie spécifiée.
-   * @param categorieId L'ID de la catégorie.
    * @param categorie La catégorie à ouvrir.
    */
-  openCategorie(categorieId: string, categorie: Categorie) {
+  openCategorie(categorie: Categorie) {
     this.navController.navigateForward(
       ['tabs', 'categories', 'liste-produits'],
       { state: { categorie } }
+    );
+  }
+
+  /**
+   * Ouvre la catégorie spécifiée.
+   * @param categorieId L'ID de la catégorie.
+   * @param categorie La catégorie à ouvrir.
+   */
+  editCategorie(categorieId: string, categorie: Categorie) {
+    this.navController.navigateForward(
+      ['tabs', 'categories', 'detail-categorie'],
+      { state: { id: categorieId, categorie } }
     );
   }
 }
